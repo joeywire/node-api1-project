df6f4c3071bdc75a77c5ae3725dcfa310f8c35b8
joeywire <61367542+joeywire@users.noreply.github.com>
2020-11-30 15:35:19 -0600

    GET endpoints built out

--- index.js
+++ index.js
@@ -9,12 +9,18 @@ const server = express();
 server.use(express.json());
 
 //Create one user for test and to define schema 
-let users = [{ id: shortid.generate(), name: "Joe", bio: "OG user"}];
+let users = [
+    { id: shortid.generate(), name: "Joe", bio: "OG user" },
+    { id: shortid.generate(), name: "Maricela", bio: "number two user" }
+];
 
 //helper functions to interact with users table 
 const User = {
     getAll() {
         return users;
+    }, 
+    getById(id) {
+        return users.find(u => u.id === id )
     }
 };
 
@@ -27,7 +33,16 @@ server.get('/api/users', (req, res) => {
     // 3 - Sent appropriate responce to client 
     res.status(200).json(users); 
 })
-
+server.get('/api/users/:id', (req, res) => {
+    //Pull id from url params 
+    const { id } = req.params;
+    const user = User.getById(id); 
+    if (user) {
+        res.status(200).json(user); 
+    } else { 
+        res.status(404).json({ message: `user not found with id of ${id}` });
+    }
+});
 //catchall enpoint 
 server.use('*', (req, res) => {
     res.status(404).json({ message: 'not found' })
