b5a0dbf49a49afedfecf3126968c463581c0cfe5
joeywire <61367542+joeywire@users.noreply.github.com>
2020-11-30 16:05:42 -0600

    Post Request built out

--- index.js
+++ index.js
@@ -21,10 +21,24 @@ const User = {
     }, 
     getById(id) {
         return users.find(u => u.id === id )
+    }, 
+    createNew(user) { 
+        const newUser = { id: shortid.generate(), ...user};
+        users.push(newUser); 
+        return newUser;  
     }
 };
 
 //endpoints 
+server.post('/api/users', (req, res) => {
+    const userFromClient = req.body; 
+    if (!userFromClient.name || !userFromClient.bio) {
+        res.status(400).json({ message: 'name and bio required!' });
+    } else {
+        const newleyCreatedUser = User.createNew(userFromClient)
+        res.status(201).json(newleyCreatedUser); 
+    }
+});
 
 server.get('/api/users', (req, res) => {
     // 1 - Gather infro from the request object 
@@ -33,6 +47,7 @@ server.get('/api/users', (req, res) => {
     // 3 - Sent appropriate responce to client 
     res.status(200).json(users); 
 })
+
 server.get('/api/users/:id', (req, res) => {
     //Pull id from url params 
     const { id } = req.params;
@@ -40,7 +55,7 @@ server.get('/api/users/:id', (req, res) => {
     if (user) {
         res.status(200).json(user); 
     } else { 
-        res.status(404).json({ message: `user not found with id of ${id}` });
+        res.status(400).json({ message: `user not found with id of ${id}` });
     }
 });
 //catchall enpoint 
